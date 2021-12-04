ffaa6a0dc86eef6ecaf20be58a78493ff74717e2
Naima Hassan <naima.hassan434@gmail.com>
2021-12-04 21:25:18 +0000

    Footer section

--- src/containers/Footer/Footer.jsx
+++ src/containers/Footer/Footer.jsx
@@ -1,7 +1,54 @@
 import React from "react";
 import "./Footer.css";
+import imgLogo from "../../assets/logo.svg";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="gpt--footer section__padding">
+      <div className="gpt--footer-heading">
+        <h1 className="gradient__text">
+          Do you want to step in to the future before others
+        </h1>
+      </div>
+
+      <div className="gpt--footer-btn">
+        <p>Request Early Access</p>
+      </div>
+
+      <div className="gpt--footer-links">
+        <div className="gpt--footer-links_logo">
+          <img src={imgLogo} alt="Logo" />
+
+          <p>
+            Crechterwoord K12 182 DK Alknjkcb, <br /> All Rights Reserved
+          </p>
+        </div>
+        <div className="gpt--footer-links_div">
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt--footer-links_div">
+          <h4>Company</h4>
+          <p>Terms & Conditions </p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt--footer-links_div">
+          <h4>Get in touch</h4>
+          <p>Crechterwoord K12 182 DK Alknjkcb</p>
+          <p>085-132567</p>
+          <p>info@payme.net</p>
+        </div>
+      </div>
+
+      <div className="gpt--footer-copyright">
+        <p>@2021 GPT-3. All rights reserved.</p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
