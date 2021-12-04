f8c80268934d3533ed13d7abe16bb5163caa8307
Naima Hassan <naima.hassan434@gmail.com>
2021-12-04 20:14:47 +0000

    cta component

--- src/components/CTA/CTA.jsx
+++ src/components/CTA/CTA.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 import "./CTA.css";
 const CTA = () => {
-  return <div>CTA</div>;
+  return (
+    <div className="gpt--cta">
+      <div className="gpt--cta-content">
+        <div>
+          <p>Request Early Access to Get Started</p>
+          <h3>
+            Register Today &amp; start exploring the endless possibilities.
+          </h3>
+        </div>
+        <div className="gpt--cta-btn">
+          <button type="button">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CTA;
